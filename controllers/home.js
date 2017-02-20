c3bae2057355ab5cfe54280c9d0c2cb48a13f124
Cory Trimm <cory.trimm@gmail.com>
2017-02-20 13:33:47 -0500

    added a bunch of partials & darksky package

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,23 @@
+const DarkSky = require('dark-sky')
+const forecast = new DarkSky('44256f903bb244724e13b4b44774c9d7')
+
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
+  var weather = forecast
+    .latitude('40.4406')
+    .longitude('-79.9959')
+    .get()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+    }).then(function() {
+        res.render('home', {
+        title: 'Home'
+      });
+    });
 };
