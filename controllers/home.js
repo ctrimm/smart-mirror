4bd34a1f5e2a273fb46cac118176e24fde0d3fcc
Cory Trimm <cory.trimm@gmail.com>
2017-02-22 08:49:45 -0500

    async parallel works with date, weather, & sorta w/ biking commute

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,49 @@
 const async = require('async');
 const DarkSky = require('dark-sky');
 const forecast = new DarkSky(process.env.DARKSKY_SECRET);
+const stocks = ['JBLU', 'HA', 'LUV', 'BCS', 'RBS', 'INSY']
+var robinhoodCreds = {
+    username: process.env.ROBINHOOD_USERNAME,
+    password: process.env.ROBINHOOD_PASSWORD
+};
+
+var robinhoodStocks = [];
+ 
+var Robinhood = require('robinhood')(robinhoodCreds, function(){
+    //Robinhood is connected and you may begin sending commands to the api.
+    for (var i = 0, len = stocks.length; i < len; i++) {
+      Robinhood.quote_data(stocks[i], function(error, response, body) {
+        if (error) {
+            console.error(error);
+            process.exit(1);
+        }
+        console.log(body.results);
+        robinhoodStocks.push(body.results);
+      });
+    }
+});
+
 const googleSecret = process.env.GOOGLEDISTANCE_SECRET;
 const homeAddress = '1111+East+Carson+Street+Pittsburgh,+PA+15203';
 const workAddress = '40+24+Street+Pittsburgh+PA+15222';
 const https = require('https');
 
-var req = https.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=1111+East+Carson+Street+Pittsburgh,+PA+15203&destinations=40+24th+street+Pittsburgh+PA&mode=bicycling&key=AIzaSyCSgIGkC1lrz1xdvWct1joe3rp1sBxEvtw', function(res) {
+// var biking = https.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+homeAddress+'&destinations='+workAddress+'&mode=bicycling&units=imperial&key='+googleSecret+'', function(res) {
+//   // Buffer the body entirely for processing as a whole.
+//   var bodyChunks = [];
+//   res.on('data', function(chunk) {
+//     // You can process streamed parts here...
+//     bodyChunks.push(chunk);
+//   }).on('end', function() {
+//     var body = Buffer.concat(bodyChunks);
+//     // console.log('BIKING: ' + body);
+//     // ...and/or process the entire body here.
+//   })
+// });
+
+// console.log('biking - ', biking);
+
+var driving = https.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+homeAddress+'&destinations='+workAddress+'&mode=driving&units=imperial&key='+googleSecret+'', function(res) {
   // Buffer the body entirely for processing as a whole.
   var bodyChunks = [];
   res.on('data', function(chunk) {
@@ -14,23 +51,10 @@ var req = https.get('https://maps.googleapis.com/maps/api/distancematrix/json?or
     bodyChunks.push(chunk);
   }).on('end', function() {
     var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
+    // console.log('DRIVING: ' + body);
     // ...and/or process the entire body here.
   })
 });
-
-var weather = forecast
-  .latitude('40.4406')
-  .longitude('-79.9959')
-  .exclude('minutely,hourly,')
-  .get()
-  .then(res => {
-
-      console.log(res.currently);
-  })
-  .catch(err => {
-      console.log(err)
-  });
 
 const days = [
   'Sunday',
@@ -57,41 +81,79 @@ const months = [
   'December'
 ];
 
-var currentDate = [];
-
-var d = new Date();
-var hour = d.getHours();
-var minutes = d.getMinutes();
-var seconds = d.getSeconds();
-var dayOfWeek = days[d.getDay()];
-var date = d.getDate();
-var month = months[d.getMonth()];
-var year = d.getFullYear();
-
-if(minutes < 10) {
-    minutes = '0'+minutes;
-}
-
-currentDate = {
-  hour: hour,
-  minutes: minutes,
-  seconds: seconds,
-  dayOfWeek: dayOfWeek,
-  date: date,
-  month: month,
-  year: year
-};
-
-console.log('weather - ', weather.currently);
+// console.log('weather - ', weather.currently);
 
 /**
  * GET /
  * Home page.
  */
-exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home',
-    date: currentDate,
-    weather: weather
+exports.index = (req, res, next) => {
+  async.parallel({
+    weather: function(callback) {
+      var weather = forecast
+        .latitude('40.4406')
+        .longitude('-79.9959')
+        .exclude('minutely,hourly,')
+        .get()
+        .then(res => {
+            callback(null, res.currently);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    },
+    currentDate: function(callback) {
+      var currentDate = [];
+
+      var d = new Date();
+      var hour = d.getHours();
+      var minutes = d.getMinutes();
+      var seconds = d.getSeconds();
+      var dayOfWeek = days[d.getDay()];
+      var date = d.getDate();
+      var month = months[d.getMonth()];
+      var year = d.getFullYear();
+
+      if(minutes < 10) {
+          minutes = '0'+minutes;
+      }
+
+      callback(null, {
+        hour: hour,
+        minutes: minutes,
+        seconds: seconds,
+        dayOfWeek: dayOfWeek,
+        date: date,
+        month: month,
+        year: year
+      });
+    },
+    biking: function(callback) {
+      var biking = https.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+homeAddress+'&destinations='+workAddress+'&mode=bicycling&units=imperial&key='+googleSecret+'', function(res) {
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        res.on('data', function(chunk) {
+          // You can process streamed parts here...
+          bodyChunks.push(chunk);
+        }).on('end', function() {
+          var body = Buffer.concat(bodyChunks);
+          console.log('BIKING: ' + body);
+          callback(null, body);
+          // ...and/or process the entire body here.
+        });
+      });
+    }
+
+  }, function(err, results) {
+      // results is now equals to: {one: 1, two: 2}
+      console.log('results', results);
+      // res.render('home', { title: 'Home', date: results.currentDate, weather: weather});
+      if (err) { return next(err); }
+      res.render('home', {
+        title: 'Home',
+        title: 'Home',
+        date: results.currentDate,
+        weather: results.weather
+      });
   });
 };
